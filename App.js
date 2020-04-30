54dfaeea81cfde0bf83d0626f1f04356b755572c
Sam Schweber <schweber97@gmail.com>
2020-04-30 14:07:11 -0400

    Setup form in react

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return(
+
+    <div id="parent-div">
+
+      <div id="title-div">
+        <h1 id="title">Survey Form</h1>
+        <p id="description">This is a fun short survey form created with React</p>
+      </div>
+
+      <div>
+        <form></form>
+      </div>
+
+    </div>
+
+  );
+}
+
+export default App;
